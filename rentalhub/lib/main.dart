4d5d1274f4f9c54b0495c9ec92af32963fd4f5f3
kisirinyasiraje <kisirinyasiraje@gmail.com>
2024-07-04 04:16:47 -0700

    Drawer

--- rentalhub/lib/main.dart
+++ rentalhub/lib/main.dart
@@ -25,6 +25,62 @@ class MyApp extends StatelessWidget {
             title: const Text('RentalHub'),
           ),
           body: const Home(),
+          drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [ const
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 215, 17, 250),
+              ),
+              child: Text(
+                'Menu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Home'),
+              onTap: () {
+                // Handle home navigation
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text('Profile'),
+              onTap: () {
+                // Handle about navigation
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.settings),
+              title: const Text('Settings'),
+              onTap: () {
+                // Handle about navigation
+              },
+            ),
+            ListTile(
+              leading: const  Icon(Icons.info),
+              title: const Text('About'),
+              onTap: () {
+                // Handle about navigation
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.logout),
+              title: const Text('Log Out'),
+              onTap: () {
+                // Handle about navigation
+              },
+            ),
+
+
+          ],
+        ),
+      ),
         ));
   }
 }
