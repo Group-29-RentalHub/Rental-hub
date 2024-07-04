00f68506f81d5e50aea8c39754b27c44aa46ad16
twesigetrejan <trejantwesige19@gmail.com>
2024-07-04 13:09:26 +0300

    linked

--- rentalhub/lib/layout/navigation.dart
+++ rentalhub/lib/layout/navigation.dart
@@ -1,62 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:rentalhub/user/profile.dart';
 
 class Navigation extends StatelessWidget {
   const Navigation({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(  
-            color:const Color.fromRGBO(70, 0, 119, 1),
-            height: 100, 
-            child: Row( 
-            
-            crossAxisAlignment: CrossAxisAlignment.end,
-            mainAxisAlignment: MainAxisAlignment.spaceAround, 
-             children: [
-            Container(
-              height: 100,
-              child: const Icon(
-                Icons.list_alt_outlined,
-                color: Colors.white,
-                size: 30.0, 
-              ),
+    return Container(
+      color: const Color.fromRGBO(70, 0, 119, 1),
+      height: 100,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Container(
+            height: 100,
+            child: const Icon(
+              Icons.list_alt_outlined,
+              color: Colors.white,
+              size: 30.0,
             ),
-            Container(
-              height: 100,
+          ),
+          Container(
+            height: 100,
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Profile()),
+                );
+              },
               child: const Icon(
                 Icons.account_circle,
                 color: Colors.white,
-                size: 30.0, 
+                size: 30.0,
               ),
             ),
-            Container(
-              height: 100,
-              child: const Icon(
-                Icons.home,
-                color: Colors.white,
-                size: 30.0, 
-              ),
-            ),
-            Container(
-              height: 100, 
-              child: Container(
-                child: const Icon(
-                Icons.menu,
-                color: Colors.white, 
-                size: 30.0, 
-              ),
-              )
-            ),
-            Container(
-              height: 100,
-              child: const Icon(
-                Icons.payment,
-                color: Colors.white,
-                size: 30.0, 
-              ),
-            ), 
-          ],
           ),
-          ) ;
+          Container(
+            height: 100,
+            child: const Icon(
+              Icons.home,
+              color: Colors.white,
+              size: 30.0,
+            ),
+          ),
+          Container(
+            height: 100,
+            child: const Icon(
+              Icons.menu,
+              color: Colors.white,
+              size: 30.0,
+            ),
+          ),
+          Container(
+            height: 100,
+            child: const Icon(
+              Icons.payment,
+              color: Colors.white,
+              size: 30.0,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
