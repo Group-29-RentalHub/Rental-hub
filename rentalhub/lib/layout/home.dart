d51a7e2fc5a98bfcaf212f9a32b660c96b364e25
Sserube Martin <berismartin256@gmail.com>
2024-07-05 00:17:41 +0300

    updated ui designs

--- rentalhub/lib/layout/home.dart
+++ rentalhub/lib/layout/home.dart
@@ -1,26 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:rentalhub/layout/navigation.dart';
 
-class Home extends StatelessWidget {
-  const Home({super.key});
 
+// Placeholder for HomePage
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromRGBO(70, 0, 119, 0),
-            Color.fromRGBO(70, 0, 119, 1),
-          ],
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-        ),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [Container(), const Navigation()],
+    return Scaffold( 
+      body: Center(
+        child: Text('Home Page'),
       ),
     );
   }
--- rentalhub/lib/layout/navigation.dart
+++ rentalhub/lib/layout/navigation.dart
@@ -1,66 +1,65 @@
-import 'package:flutter/material.dart';
-import 'package:rentalhub/user/notifications.dart';
-import 'package:rentalhub/user/profile.dart';
+// import 'package:flutter/material.dart';
+// import 'package:rentalhub/user/notifications.dart';
+//  import 'package:rentalhub/user/profile.dart';
 
-class Navigation extends StatelessWidget {
-  const Navigation({super.key});
+// class Navigation extends StatelessWidget {
+//   const Navigation({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(  
-            color:const Color.fromRGBO(70, 0, 119, 1),
-            height: 50, 
-            child: Row( 
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(   
+//             height: 50, 
+//             child: Row( 
             
-            crossAxisAlignment: CrossAxisAlignment.end,
-            mainAxisAlignment: MainAxisAlignment.spaceAround, 
-             children: [ 
+//             crossAxisAlignment: CrossAxisAlignment.end,
+//             mainAxisAlignment: MainAxisAlignment.spaceAround, 
+//              children: [ 
            
-              FilledButton( 
-               onPressed: () {
-                 Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Profile()),
-                );
-              }, 
-                child: const Icon(
-                Icons.flash_on_sharp,
-                color: Colors.white,
-                size: 30.0, 
+//               FilledButton( 
+//                onPressed: () {
+//                  Navigator.push(
+//                   context,
+//                   MaterialPageRoute(builder: (context) => Profile()),
+//                 );
+//               }, 
+//                 child: const Icon(
+//                 Icons.flash_on_sharp,
+//                 color: Colors.white,
+//                 size: 30.0, 
                 
-              ) 
+//               ) 
               
-            ), 
-            FilledButton( 
-              child: const Icon(
-                Icons.home,
-                color: Colors.white,
-                size: 30.0, 
+//             ), 
+//             FilledButton( 
+//               child: const Icon(
+//                 Icons.home,
+//                 color: Colors.white,
+//                 size: 30.0, 
                 
-              ),
-              onPressed: () {
-                 Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Profile()),
-                );
-              },
-            ), 
-            FilledButton( 
-              child: const Icon(
-                Icons.notifications,
-                color: Colors.white,
-                size: 30.0, 
+//               ),
+//               onPressed: () {
+//                  Navigator.push(
+//                   context,
+//                   MaterialPageRoute(builder: (context) => Profile()),
+//                 );
+//               },
+//             ), 
+//             FilledButton( 
+//               child: const Icon(
+//                 Icons.notifications,
+//                 color: Colors.white,
+//                 size: 30.0, 
                 
-              ),
-              onPressed: () {
-                 Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Profile()),
-                );
-              },
-            ), 
-          ],
-          ),
-          ) ;
-  }
-}
+//               ),
+//               onPressed: () {
+//                  Navigator.push(
+//                   context,
+//                   MaterialPageRoute(builder: (context) => NotificationHistoryApp()),
+//                 );
+//               },
+//             ), 
+//           ],
+//           ),
+//           ) ;
+//   }
+// }
