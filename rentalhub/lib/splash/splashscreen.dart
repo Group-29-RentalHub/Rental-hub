6386cc8cf48221dc71e478b8f609b8b182f87954
kisirinyasiraje <kisirinyasiraje@gmail.com>
2024-07-09 09:28:26 -0700

    splash screen

--- rentalhub/lib/splash/splashscreen.dart
+++ rentalhub/lib/splash/splashscreen.dart
@@ -1,34 +1,27 @@
+
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.purple[900],
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Image.asset(
-            //   'image/prentalhub.png', // Replace with your logo image
-            //   height: 150,
-            // ),
-            
-           
-            const SizedBox(height: 20),
-            const Text(
-              'Rental Hub',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('image/purple.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+     
+              const CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
               ),
-            ),
-            const SizedBox(height: 10),
-            const CircularProgressIndicator(
-              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
