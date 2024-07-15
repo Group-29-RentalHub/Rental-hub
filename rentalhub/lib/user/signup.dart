ece41aba4532000e2f692b4b31a891435bfa9208
twesigetrejan <trejantwesige19@gmail.com>
2024-07-15 13:31:05 +0300

    firebase

--- rentalhub/lib/user/signup.dart
+++ rentalhub/lib/user/signup.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:rentalhub/user/login.dart';
 
 class SignupPage extends StatefulWidget {
@@ -55,6 +56,50 @@ class _SignupPageState extends State<SignupPage> {
       return 'Password must contain at least one special character';
     }
     return null;
+  }
+
+  Future<void> _signup() async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: _emailController.text.trim(),
+        password: _passwordController.text.trim(),
+      );
+      // Signup successful, you can navigate to the home page or show a success message
+      print('Signup successful!');
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginPage()), // Replace with your desired page
+      );
+    } on FirebaseAuthException catch (e) {
+      String message = '';
+      if (e.code == 'email-already-in-use') {
+        message = 'The email address is already in use.';
+      } else if (e.code == 'invalid-email') {
+        message = 'The email address is invalid.';
+      } else if (e.code == 'operation-not-allowed') {
+        message = 'Email/password accounts are not enabled.';
+      } else if (e.code == 'weak-password') {
+        message = 'The password is too weak.';
+      } else {
+        message = 'An error occurred. Please try again later.';
+      }
+      // Show error message
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Signup Failed'),
+          content: Text(message),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'),
+            ),
+          ],
+        ),
+      );
+    }
   }
 
   @override
@@ -144,10 +189,7 @@ class _SignupPageState extends State<SignupPage> {
               ElevatedButton(
                 onPressed: () {
                   if (_formKey.currentState?.validate() ?? false) {
-                    // Implement signup logic here
-                    // Access user data using the controllers
-                    // _usernameController.text, _emailController.text, _passwordController.text
-                    print('Signup successful!');
+                    _signup();
                   }
                 },
                 child: Text('Signup'),
--- rentalhub/macos/Flutter/GeneratedPluginRegistrant.swift
+++ rentalhub/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,15 +5,12 @@
 import FlutterMacOS
 import Foundation
 
-<<<<<<< HEAD
+import file_selector_macos
+import firebase_auth
 import firebase_core
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-=======
-import file_selector_macos
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
->>>>>>> beris
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
 }
