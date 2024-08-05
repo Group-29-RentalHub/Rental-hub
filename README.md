2f72fb5fe81adaeadb19d93bc0e5abfa515ee20e
kisirinyasiraje <kisirinyasiraje@gmail.com>
2024-08-05 02:06:30 -0700

    Removed old project files

--- README.md
+++
@@ -1,3 +0,0 @@
-# Rental-hub
-This is our repository  for developing a rental mobile app with flutter
-
--- rentalhub/android/app/src/main/kotlin/com/example/rentalhub/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.rentalhub
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
