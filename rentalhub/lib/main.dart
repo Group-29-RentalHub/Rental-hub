bef0679d061aa2c1febcd075354afa39ca93d5b7
KisirinyaSirajje <kisirinyasiraje@gmail.com>
2024-08-09 07:23:00 -0700

    Updated the search Button to filter from saved files in firebase

--- rentalhub/lib/main.dart
+++ rentalhub/lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:halls/hostel_lists.dart';
-import 'landingpage.dart'; // Ensure this is your booking/registration page
+import 'landingpage.dart';
 import 'notifications.dart';
-import 'profile.dart'; // Import profile page
+import 'profile.dart';
 import 'home.dart';
 import 'profile_form.dart';
-// import 'about.dart';
 import 'settings.dart';
 import 'login.dart';
 import 'signup.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -29,14 +29,14 @@ class RentalHub extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/', // Ensure this is set to '/'
+      initialRoute: '/',
       routes: {
-        '/': (context) => const AuthWrapper(), // Use AuthWrapper to handle authentication
+        '/': (context) => const AuthWrapper(),
         '/notifications': (context) => const NotificationHistoryPage(),
-        '/profile': (context) => Profile(userId: FirebaseAuth.instance.currentUser!.uid), // Pass userId to Profile
+        '/profile': (context) => Profile(userId: FirebaseAuth.instance.currentUser!.uid),
         '/profileForm': (context) => ProfileFormPage(
               onSubmit: () {
-                navigateToHomePage(context); // Pass callback to ProfileFormPage
+                navigateToHomePage(context);
               },
             ),
         '/about': (context) => const HostelListingsPage(),
@@ -63,11 +63,11 @@ class AuthWrapper extends StatelessWidget {
       stream: FirebaseAuth.instance.authStateChanges(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return const CircularProgressIndicator(); // Display a loading indicator while waiting for authentication state
+          return const CircularProgressIndicator();
         } else if (snapshot.hasData) {
-          return const MainPage(); // If the user is authenticated, show the main page
+          return const MainPage();
         } else {
-          return const LoginPage(); // If the user is not authenticated, show the login page
+          return const LoginPage();
         }
       },
     );
@@ -85,12 +85,12 @@ class _MainPageState extends State<MainPage> {
   bool _isSearching = false;
   final TextEditingController _searchController = TextEditingController();
   int _currentIndex = 0;
-  String _title = 'RentalHub'; // Default title
+  String _title = 'RentalHub';
 
   final List<Widget> _pages = [
     ForYouPage(),
     const NotificationHistoryPage(),
-    Profile(userId: FirebaseAuth.instance.currentUser!.uid), // Pass userId to Profile
+    Profile(userId: FirebaseAuth.instance.currentUser!.uid),
     const LandingPage(),
     const LoginPage(),
   ];
@@ -136,6 +136,15 @@ class _MainPageState extends State<MainPage> {
     Navigator.pushReplacementNamed(context, '/login');
   }
 
+  void _performSearch(String query) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => SearchResultsPage(searchQuery: query),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -155,8 +164,7 @@ class _MainPageState extends State<MainPage> {
                 ),
                 style: const TextStyle(color: Colors.white),
                 onSubmitted: (query) {
-                  // Perform search operation
-                  print('Search query: $query');
+                  _performSearch(query);
                   setState(() {
                     _isSearching = false;
                   });
@@ -215,10 +223,10 @@ class _MainPageState extends State<MainPage> {
                 leading: const Icon(Icons.person),
                 title: const Text('Profile'),
                 onTap: () {
-                  Navigator.pop(context); // Close the drawer before navigating
+                  Navigator.pop(context);
                   setState(() {
                     _currentIndex = 2;
-                    _title = 'Profile'; // Update title when navigating from drawer
+                    _title = 'Profile';
                   });
                 },
               ),
@@ -226,26 +234,24 @@ class _MainPageState extends State<MainPage> {
                 leading: const Icon(Icons.settings),
                 title: const Text('Settings'),
                 onTap: () {
-                  Navigator.pop(context); // Close the drawer before navigating
-                  Navigator.pushNamed(
-                      context, '/settings'); // Navigate to SettingsPage
+                  Navigator.pop(context);
+                  Navigator.pushNamed(context, '/settings');
                 },
               ),
               ListTile(
                 leading: const Icon(Icons.info),
                 title: const Text('About'),
                 onTap: () {
-                  Navigator.pop(context); // Close the drawer before navigating
-                  Navigator.pushNamed(
-                      context, '/about'); // Navigate to AboutPage
+                  Navigator.pop(context);
+                  Navigator.pushNamed(context, '/about');
                 },
               ),
               ListTile(
                 leading: const Icon(Icons.logout),
                 title: const Text('Log Out'),
                 onTap: () {
-                  Navigator.pop(context); // Close the drawer
-                  _logout(); // Call the logout function
+                  Navigator.pop(context);
+                  _logout();
                 },
               ),
             ],
@@ -274,11 +280,96 @@ class _MainPageState extends State<MainPage> {
             label: 'Explore',
           ),
         ],
-        selectedItemColor: Colors.grey, // Color for selected item
-        unselectedItemColor: Color.fromRGBO(70, 0, 119, 1), // Color for unselected items
-        // backgroundColor: Color.fromRGBO(70, 0, 119, 1),
+        selectedItemColor: Colors.grey,
+        unselectedItemColor: Color.fromRGBO(70, 0, 119, 1),
         selectedLabelStyle: TextStyle(color: Colors.purple),
-        backgroundColor: const Color.fromRGBO(70, 0, 119, 1),// Background color of the BottomNavigationBar
+        backgroundColor: const Color.fromRGBO(70, 0, 119, 1),
+      ),
+    );
+  }
+}
+
+class SearchResultsPage extends StatelessWidget {
+  final String searchQuery;
+
+  const SearchResultsPage({Key? key, required this.searchQuery}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Search Results for "$searchQuery"'),
+        backgroundColor: const Color.fromRGBO(70, 0, 119, 1),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('hostels')
+            .where('name', isGreaterThanOrEqualTo: searchQuery.toLowerCase())
+            .where('name', isLessThan: searchQuery.toLowerCase() + 'z')
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return const Center(child: Text('No results found'));
+          }
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              var hostel = snapshot.data!.docs[index];
+              return HostelCard(hostel: hostel);
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class HostelCard extends StatelessWidget {
+  final QueryDocumentSnapshot hostel;
+
+  const HostelCard({Key? key, required this.hostel}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.network(
+            hostel['images'][0] ?? 'https://via.placeholder.com/150',
+            height: 200,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              hostel['name'] ?? 'Unnamed Hostel',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
+          ButtonBar(
+            alignment: MainAxisAlignment.start,
+            children: [
+              TextButton(
+                child: Text('View Details'),
+                onPressed: () {
+                  // Navigate to hostel details page
+                },
+              ),
+              TextButton(
+                child: Text('Book Now'),
+                onPressed: () {
+                  // Handle booking logic
+                },
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
