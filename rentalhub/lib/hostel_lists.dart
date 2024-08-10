c3a519583c63469e91aafb2f0ee217fccc59cdbb
Sserube Martin <berismartin256@gmail.com>
2024-08-11 00:13:07 +0300

    error creating hostels

--- rentalhub/lib/hostel_lists.dart
+++ rentalhub/lib/hostel_lists.dart
@@ -58,7 +58,7 @@ class _HostelListingsPageState extends State<HostelListingsPage> {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
+            return Center(child: Text('Error:: ${snapshot.error}'));
           } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
             return const Center(child: Text('No hostels found.'));
           }
